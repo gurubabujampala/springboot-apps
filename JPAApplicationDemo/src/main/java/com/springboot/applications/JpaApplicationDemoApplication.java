6ea83de1532a11f2d1c62cc2057120c39e1001ea
Gurubabu Jampala <j.gurubabu@gmail.com>
2019-05-19 12:18:00 +0530

    JAP application demo

---
+++ JPAApplicationDemo/src/main/java/com/springboot/applications/JpaApplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaApplicationDemoApplication.class, args);
+	}
+
+}
